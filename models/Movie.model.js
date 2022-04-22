d2c97818ca53ff5a4b9de70de615802fe4d447e3
joaonacho <joaonacho@gmail.com>
2022-04-22 17:17:02 +0100

    timestamps in movie & review model

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -1,17 +1,22 @@
 const { Schema, model } = require("mongoose");
 
-const movieSchema = new Schema({
-  title: String,
-  genres: [{ id: Number, name: String }],
-  poster_path: String,
-  overview: String,
-  tagline: String,
-  vote_average: { type: Number, min: 0, max: 10 },
-  release_date: String,
-  runtime: Number,
-  id: Number,
-  imdb_id: String,
-  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
-});
+const movieSchema = new Schema(
+  {
+    title: String,
+    genres: [{ id: Number, name: String }],
+    poster_path: String,
+    overview: String,
+    tagline: String,
+    vote_average: { type: Number, min: 0, max: 10 },
+    release_date: String,
+    runtime: Number,
+    id: Number,
+    imdb_id: String,
+    reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = model("Movie", movieSchema);
--- models/Review.model.js
+++ models/Review.model.js
@@ -1,10 +1,15 @@
 const { Schema, model } = require("mongoose");
 
-const reviewSchema = new Schema({
-  author: { type: Schema.Types.ObjectId, ref: "User" },
-  movie: { type: Schema.Types.ObjectId, ref: "Movie" },
-  review: String,
-  rating: { type: Number, min: 0, max: 10, required: true },
-});
+const reviewSchema = new Schema(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User" },
+    movie: { type: Schema.Types.ObjectId, ref: "Movie" },
+    review: String,
+    rating: { type: Number, min: 0, max: 10, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = model("Review", reviewSchema);

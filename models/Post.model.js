fc0ac10b64c9fde65af3f49e0e5c929027af2d13
joaonacho <joaonacho@gmail.com>
2022-05-03 21:39:30 +0100

    feed done

--- models/Post.model.js
+++ models/Post.model.js
@@ -3,7 +3,11 @@ const { Schema, model } = require("mongoose");
 const postSchema = new Schema(
   {
     author: { type: Schema.Types.ObjectId, ref: "User" },
-    poster: String,
+    poster: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dxxmsbtrt/image/upload/v1651586382/MovieScreen/dfcxxx8se4bayk6tqbtq.jpg",
+    },
     content: String,
     likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
     comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -7,12 +7,25 @@ router.get("/feed/:username", async (req, res) => {
   try {
     const { username } = req.params;
 
-    const foundUser = await User.findOne({ username: username }).populate({
-      path: "follows",
-      populate: { path: "followers" },
+    const loggedUser = await User.findOne({ username: username }).populate(
+      "follows"
+    );
+    const { follows } = loggedUser;
+
+    const allPosts = await Post.find().populate("author");
+    const loggedUserPosts = allPosts.filter(
+      (post) => post.author.username === loggedUser.username
+    );
+
+    let followsPosts = [];
+    follows.forEach((follow) => {
+      let foundFollowPosts = allPosts.filter(
+        (post) => post.author.username === follow.username
+      );
+      followsPosts.push(...foundFollowPosts);
     });
 
-    res.status(200).json(foundUser);
+    res.status(200).json([...loggedUserPosts, ...followsPosts]);
   } catch (error) {
     console.error({ message: error.message });
     res.status(500).json({ error });
@@ -22,7 +35,25 @@ router.get("/feed/:username", async (req, res) => {
 //POST create a post
 router.post("/create-post", async (req, res) => {
   try {
-    console.log(req.body);
+    const { poster, content, likes, comments } = req.body.post;
+    const { author } = req.body.post;
+
+    const newPost = await Post.create({
+      author,
+      poster,
+      content,
+      likes,
+      comments,
+    });
+
+    console.log(newPost);
+    await User.findByIdAndUpdate(
+      author,
+      { $push: { posts: newPost._id } },
+      { new: true }
+    );
+
+    res.status(200).json(newPost);
   } catch (error) {
     res.status(500).json({ error });
   }

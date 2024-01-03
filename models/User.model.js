d37cb6b6b7ba2085063ccf5d5a073043eed3f268
andregn26 <andregn26@gmail.com>
2024-01-03 11:40:25 +0000

    changed origin

--- models/User.model.js
+++ models/User.model.js
@@ -1,36 +1,35 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    match: [/^\S+@\S+\.\S+$/, "You need to use a valid email."],
-    required: true,
-    lowercase: true,
-    trim: true,
-  },
-  profileImg: {
-    type: String,
-    default:
-      "https://res.cloudinary.com/dxxmsbtrt/image/upload/v1650390383/MovieScreen/Users/avatar-profile_af3anp.webp",
-  },
-  country: String,
-  genres: [String],
-  about: { type: String, default: "I love movies" },
-  followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  follows: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  favourites: [{ type: Schema.Types.ObjectId, ref: "Movie" }],
-  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
-  posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		unique: true,
+		match: [/^\S+@\S+\.\S+$/, "You need to use a valid email."],
+		required: true,
+		lowercase: true,
+		trim: true,
+	},
+	profileImg: {
+		type: String,
+		default: "https://res.cloudinary.com/dxxmsbtrt/image/upload/v1650390383/MovieScreen/Users/avatar-profile_af3anp.webp",
+	},
+	country: String,
+	genres: [String],
+	about: { type: String, default: "I love movies" },
+	followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+	follows: [{ type: Schema.Types.ObjectId, ref: "User" }],
+	favourites: [{ type: Schema.Types.ObjectId, ref: "Movie" }],
+	reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+	posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
 });
 
 const User = model("User", userSchema);
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,89 +8,89 @@ const router = express.Router();
 
 //Signup
 router.post("/signup", async (req, res) => {
-  try {
-    const { username, email, password, country, genres, profileImg } = req.body;
+	try {
+		const { username, email, password, country, genres, profileImg } = req.body;
 
-    //Info for confirmation
-    if (!email || !password || !username) {
-      res.status(400).json({ message: "missing fields" });
-      return;
-    }
+		//Info for confirmation
+		if (!email || !password || !username) {
+			res.status(400).json({ message: "missing fields" });
+			return;
+		}
 
-    //Checks if username already exists in DB
-    const foundUser = await User.findOne({ username: username });
-    if (foundUser) {
-      res.status(400).json({ message: "user already exists" });
-      return;
-    }
+		//Checks if username already exists in DB
+		const foundUser = await User.findOne({ username: username });
+		if (foundUser) {
+			res.status(400).json({ message: "user already exists" });
+			return;
+		}
 
-    //Add salt to password
-    const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(password, salt);
+		//Add salt to password
+		const salt = bcrypt.genSaltSync(10);
+		const hashedPassword = bcrypt.hashSync(password, salt);
 
-    //Create user if all conditions are passed
-    const createdUser = await User.create({
-      email,
-      username,
-      country,
-      genres,
-      password: hashedPassword,
-      profileImg,
-    });
+		//Create user if all conditions are passed
+		const createdUser = await User.create({
+			email,
+			username,
+			country,
+			genres,
+			password: hashedPassword,
+			profileImg,
+		});
 
-    res.status(200).json({
-      email: createdUser.email,
-      username: createdUser.username,
-      _id: createdUser._id,
-    });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+		res.status(200).json({
+			email: createdUser.email,
+			username: createdUser.username,
+			_id: createdUser._id,
+		});
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 });
 
 //LOGIN
 router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+	try {
+		const { email, password } = req.body;
 
-    if (!email || !password) {
-      res.status(400).json({ message: "missing fields" });
-      return;
-    }
+		if (!email || !password) {
+			res.status(400).json({ message: "missing fields" });
+			return;
+		}
 
-    const foundUser = await User.findOne({ email: email });
+		const foundUser = await User.findOne({ email: email });
 
-    if (!foundUser) {
-      res.status(401).json({ message: "invalid login" });
-      return;
-    }
+		if (!foundUser) {
+			res.status(401).json({ message: "invalid login" });
+			return;
+		}
 
-    const correctPassword = bcrypt.compareSync(password, foundUser.password);
+		const correctPassword = bcrypt.compareSync(password, foundUser.password);
 
-    if (!correctPassword) {
-      res.status(401).json({ message: "invalid login" });
-      return;
-    }
+		if (!correctPassword) {
+			res.status(401).json({ message: "invalid login" });
+			return;
+		}
 
-    const authToken = jwt.sign(
-      {
-        _id: foundUser._id,
-        email: foundUser.email,
-        username: foundUser.username,
-        profileImg: foundUser.profileImg,
-      },
-      process.env.TOKEN_SECRET,
-      { algorithm: "HS256", expiresIn: "6h" }
-    );
+		const authToken = jwt.sign(
+			{
+				_id: foundUser._id,
+				email: foundUser.email,
+				username: foundUser.username,
+				profileImg: foundUser.profileImg,
+			},
+			process.env.TOKEN_SECRET,
+			{ algorithm: "HS256", expiresIn: "6h" }
+		);
 
-    res.status(200).json({ authToken });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
+		res.status(200).json({ authToken });
+	} catch (error) {
+		res.status(500).json({ error });
+	}
 });
 
 router.get("/verify", isAuthenticated, (req, res) => {
-  res.status(200).json(req.payload);
+	res.status(200).json(req.payload);
 });
 
 module.exports = router;
